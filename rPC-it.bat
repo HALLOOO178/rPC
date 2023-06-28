362d2d1662c300fde4057c81d1a60f960474e3c2
Hallo <118673681+HALLOOO178@users.noreply.github.com>
2023-06-28 11:34:42 +0200

    Add files via upload

---
+++ rPC-it.bat
@@ -0,0 +1,4 @@
+@echo off
+echo il pc ora è accessiblile sul wi-fi
+cd \
+python -m http.server -d 192.168.178.41
---
+++ rPC.bat
@@ -0,0 +1,9 @@
+@echo off
+echo now the pc is alvelible on wifi
+cd \
+python -m http.server -d 192.168.178.41
+X:
+cd \
+winbrew.bat
+pause
+explorer
